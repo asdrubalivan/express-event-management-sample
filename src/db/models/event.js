d5403bf5ae291824496eb1da62d2405fc5ff62a5
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2018-02-11 00:13:34 -0400

    Adding new fields for event table

--- src/db/models/event.js
+++ src/db/models/event.js
@@ -1,3 +1,15 @@
+const isLongitudeValid = (val) => {
+  if (Math.abs(val) >= 180) {
+    throw new Error('Longitude is not valid');
+  }
+};
+
+const isLatitudeValid = (val) => {
+  if (Math.abs(val) >= 90) {
+    throw new Error('Latitude is not valid');
+  }
+};
+
 module.exports = (instance, Sequelize) => {
   const model = instance.define('event', {
     name: {
@@ -5,6 +17,24 @@ module.exports = (instance, Sequelize) => {
     },
     venue: {
       type: Sequelize.STRING,
+    },
+    begin_date: {
+      type: Sequelize.DATE,
+    },
+    end_date: {
+      type: Sequelize.DATE,
+    },
+    longitude: {
+      type: Sequelize.DECIMAL(10, 7),
+      validate: {
+        isLongitudeValid,
+      },
+    },
+    latitude: {
+      type: Sequelize.DECIMAL(9, 7),
+      validate: {
+        isLatitudeValid,
+      },
     },
   });
   return model;
